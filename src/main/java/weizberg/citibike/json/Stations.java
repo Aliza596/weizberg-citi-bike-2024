fa5283ed627c2d6edb33d33cf56a97022947b395
Alison Weizberg <weizberga@gmail.com>
2024-12-26 12:27:39 -0500

    fixed checkstyle

--- src/main/java/weizberg/citibike/json/Stations.java
+++ src/main/java/weizberg/citibike/json/Stations.java
@@ -8,11 +8,8 @@ public class Stations {
 
     //CHECKSTYLE:OFF
     public String station_id;
-    //CHECKSTYLE:ON
-
 
     //from status
-    //CHECKSTYLE:OFF
     public int num_bikes_available;
     public int num_docks_available;
     //CHECKSTYLE:ON
