cd09b98eb5efa92d3815d6684ea8ff4f25845b22
Alison Weizberg <weizberga@gmail.com>
2024-12-29 12:39:23 -0500

    fixed the merge so that teh bikes and docks is being updated properly

--- src/main/java/weizberg/citibike/json/DataCollection.java
+++ src/main/java/weizberg/citibike/json/DataCollection.java
@@ -14,28 +14,19 @@ public class DataCollection {
 
     public void mergeData(DataCollection collection) {
 
+        for (int i = 0; i < this.data.stations.length; i++) {
+            Station station = this.data.stations[i];
+            stationsMap.put(station.station_id, station);
+        }
 
         for (int i = 0; i < collection.data.stations.length; i++) {
             Station status = collection.data.stations[i];
             Station station = stationsMap.get(status.station_id);
 
-
             if (station != null) {
                 station.num_bikes_available = status.num_bikes_available;
                 station.num_docks_available = status.num_docks_available;
-                System.out.println("Bikes" + station.num_bikes_available);
-                System.out.println("Docks" + station.num_docks_available);
             }
-        }
-
-
-        for (int i = 0; i < this.data.stations.length; i++) {
-            Station station = this.data.stations[i];
-            stationsMap.put(station.station_id, station);
-
-
-
-
         }
     }
 
