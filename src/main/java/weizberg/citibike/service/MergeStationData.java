25e99db15afc4789256bca9ac377d2bebd436883
Alison Weizberg <weizberga@gmail.com>
2025-01-09 00:28:19 -0500

    fixed the cache

--- src/main/java/weizberg/citibike/service/MergeStationData.java
+++ src/main/java/weizberg/citibike/service/MergeStationData.java
@@ -28,16 +28,17 @@ public class MergeStationData {
     }
 
     public Map<String, Station> mergeData() {
-        Data stationStatus = stationsCache.getStationResponse();
-        DataCollection stationInfo = citibikeService.stationLocation().blockingGet();
+
+        DataCollection stationStatus = citibikeService.stationStatus().blockingGet();
+        Data stationInfo = stationsCache.getStationResponse();
 
         Map<String, Station> stationMap = new HashMap<>();
 
-        for (Station station : stationStatus.stations) {
+        for (Station station : stationStatus.data.stations) {
             stationMap.put(station.station_id, station);
         }
 
-        for (Station stationLocation : stationInfo.data.stations) {
+        for (Station stationLocation : stationInfo.stations) {
             Station stationInformation = stationMap.get(stationLocation.station_id);
 
             if (stationLocation != null) {
@@ -48,6 +49,19 @@ public class MergeStationData {
             }
             stationMap.put(stationLocation.station_id, stationLocation);
         }
+
+/*        for (int i = 0; i < stationInfo.stations.length; i++) {
+            Station stationLocation = stationInfo.stations[i];
+            Station stationInformation = stationMap.get(stationLocation.station_id);
+
+            if (stationLocation != null) {
+                stationLocation.num_docks_available = stationInformation.num_docks_available;
+                stationLocation.num_bikes_available = stationInformation.num_bikes_available;
+            } else {
+                stationMap.put(stationLocation.station_id, stationLocation);
+            }
+            stationMap.put(stationLocation.station_id, stationLocation);
+        }*/
 
         return stationMap;
 
